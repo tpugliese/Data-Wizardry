b57c633e133fa1eb17880138b828f2c9c9cde94a
Tyler Pugliese <tpugliese@fastly.com>
2015-08-12 16:19:45 -0700

    Create LendingFlub.sql
    from failed conversation with lending club.  showing a bit of snark

---
+++ LendingFlub.sql
@@ -0,0 +1,19 @@
+DROP TABLE lending_club;
+CREATE TABLE lending_club
+(proposed_date date NOT NULL,
+ individual char(24),
+  proposed_time TIME,
+  call_made TIME,
+  length_min numeric,
+  result varchar(255)
+) WITH 
+(OIDS=FALSE
+);
+
+INSERT INTO lending_club VALUES
+('2015-07-20', 'Anna Selina', '11:00:00', '11:05:00', 0, 'Technical difficulties-Rescheduled'),
+('2015-07-21', 'Anna Selina', '15:00:00', '15:10:00', 28, 'Pleasant chat and referred to Peggy Chong'),
+('2015-07-22', 'Stan Hsing', '12:17:00', '12:17:00', .5, 'Stan was late Tyler had commitment at 12:30-Rescheduled'),
+('2015-07-23', 'Stan Hsing', '14:00:00', NULL, 0, 'Stan never called');
+
+Select * from lending_club;
