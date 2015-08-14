96188a2d1a7bab3e7f94bf0582b1061ad6e73224
Tyler Pugliese <tpugliese@fastly.com>
2015-08-13 22:01:31 -0700

    Update and rename LendingFlub.sql to Create_Tables.sql

--- Create_Tables.sql
+++ Create_Tables.sql
@@ -1,5 +1,5 @@
-DROP TABLE lending_club;
-CREATE TABLE lending_club
+DROP TABLE fencing_club;
+CREATE TABLE fencing_club
 (proposed_date date NOT NULL,
  individual char(24),
   proposed_time TIME,
@@ -10,10 +10,10 @@ CREATE TABLE lending_club
 (OIDS=FALSE
 );
 
-INSERT INTO lending_club VALUES
+INSERT INTO fencing_club VALUES
 ('2015-07-20', 'Anna Selina', '11:00:00', '11:05:00', 0, 'Technical difficulties-Rescheduled'),
 ('2015-07-21', 'Anna Selina', '15:00:00', '15:10:00', 28, 'Pleasant chat and referred to Peggy Chong'),
 ('2015-07-22', 'Stan Hsing', '12:17:00', '12:17:00', .5, 'Stan was late Tyler had commitment at 12:30-Rescheduled'),
 ('2015-07-23', 'Stan Hsing', '14:00:00', NULL, 0, 'Stan never called');
 
-Select * from lending_club;
+Select * from fencing_club;
